721e52fcb14d8134394264586a4fe92e73574059
Vladimir Panteleev <vladimir@thecybershadow.net>
2017-02-02 16:31:40 +0000

    tests/msvc14: Use /link /DEBUG:FASTLINK

--- tests/msvc14/build.bat
+++ tests/msvc14/build.bat
@@ -2,6 +2,8 @@
 cd /d "%~dp0"
 call ..\..\scripts\clear_env.cmd
 
+rem https://github.com/VerySleepy/verysleepy/issues/28
+
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-cl /MD /Z7 /DEBUG program.c
+cl /MD /Z7 program.c /link /DEBUG:FASTLINK
