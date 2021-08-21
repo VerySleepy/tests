ce4c6ce5455bfc84bfbc563d52dfde5df893b8fc
Vladimir Panteleev <vladimir@thecybershadow.net>
2021-08-21 10:46:58 +0000

    tests/dmd-*: Exit on error

--- tests/dmd-32mscoff/build.bat
+++ tests/dmd-32mscoff/build.bat
@@ -4,6 +4,7 @@ cmd /c ..\..\scripts\toolchains_download.cmd
 call ..\..\scripts\clear_env.cmd
 
 "%~dp0\..\..\toolchains\dmd.2.073.0.windows\dmd2\windows\bin\dmd" program.d -c -betterC -m32mscoff -g
+if %ERRORLEVEL% neq 0 exit /b 1
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
