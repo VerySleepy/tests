133e6f1e88440251357f0f2fb72d72692fcd46f9
Todd Hartman <mojo@spamcop.net>
2022-04-07 15:24:33 -0500

    script cleanup: clear saved config to ensure that default prefs are used (and known)

--- tests/dmd-32mscoff/test.bat
+++ tests/dmd-32mscoff/test.bat
@@ -6,6 +6,10 @@ set SLEEPY_SILENT_CRASH=1
 
 for %%b in (32 64) do (
 	echo Testing %%b
+
+	rem  Discard any saved config options.
+	call ..\..\scripts\clear_config
+
 	if exist program-%%b.sleepy del program-%%b.sleepy
 	call ..\..\scripts\sleepy%%b /r:program.exe /o:program-%%b.sleepy
 	if !ERRORLEVEL! neq 0 exit /b 1
